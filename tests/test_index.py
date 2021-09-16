d2329f0b9618d0fe04bc57b1e57534aa3ab32116
Lakshmipriya1411 <88259438+Lakshmipriya1411@users.noreply.github.com>
2021-09-16 06:41:30 +0530

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_index():
-    response = requests.get("http://127.0.0.1:5000/")
+    response = requests.get("http://0.0.0.0:8080/")
     assert response.status_code == 200
