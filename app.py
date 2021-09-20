25c5de4d2520f989ca65f95c2f455793a6417772
Lakshmi1411 <lakshmi.gundlapalli@sjsu.edu>
2021-09-19 20:43:20 -0700

    msg

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from flask import url_for
 from flask_bootstrap import Bootstrap
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
+# from flask_mysqldb import MySQL
+# import MySQLdb.cursors
 import re
 app = Flask(__name__)
 xray_recorder.configure(service='Bitcoin application')
 XRayMiddleware(app, xray_recorder)
 bootstrap = Bootstrap(app)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'bitcoin'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'root'
+# app.config['MYSQL_DB'] = 'bitcoin'
   
-mysql = MySQL(app)
+# mysql = MySQL(app)
 @app.route('/')
 @app.route('/index')
 def index():
@@ -40,10 +40,10 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE username = % s AND password = % s', (username, password, ))
-        account = cursor.fetchone()
-        if account:
+        # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        # cursor.execute('SELECT * FROM user WHERE username = % s AND password = % s', (username, password, ))
+        # account = cursor.fetchone()
+        if True:
             # session['loggedin'] = True
             # session['id'] = account['id']
             # session['username'] = account['username']
@@ -63,21 +63,21 @@ def signup():
         firstname = request.form['firstname']
         lastname = request.form['lastname']
         email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM user WHERE username = % s', (username, ))
-        account = cursor.fetchone()
-        if account:
-            msg = 'Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form !'
-        else:
-            cursor.execute('INSERT INTO user VALUES (NULL,%s,%s, % s, % s, % s)', (firstname,lastname,username, password, email, ))
-            mysql.connection.commit()
-            msg = 'You have successfully registered !'
-    elif request.method == 'POST':
-        msg = 'Please fill out the form !'
+        # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        # cursor.execute('SELECT * FROM user WHERE username = % s', (username, ))
+        # account = cursor.fetchone()
+        # if account:
+            # msg = 'Account already exists !'
+        # elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            # msg = 'Invalid email address !'
+        # elif not re.match(r'[A-Za-z0-9]+', username):
+            # msg = 'Username must contain only characters and numbers !'
+        # elif not username or not password or not email:
+            # msg = 'Please fill out the form !'
+        # else:
+            # cursor.execute('INSERT INTO user VALUES (NULL,%s,%s, % s, % s, % s)', (firstname,lastname,username, password, email, ))
+            # mysql.connection.commit()
+        msg = 'You have successfully registered !'
+    # elif request.method == 'POST':
+        # msg = 'Please fill out the form !'
     return render_template('signup.html', msg = msg)                  
