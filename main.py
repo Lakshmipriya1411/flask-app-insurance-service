fa9b1a07599cdd88cca9cc40de195eeb7b56ef27
Lakshmipriya1411 <88259438+Lakshmipriya1411@users.noreply.github.com>
2021-10-11 00:13:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,96 +1,39 @@
-import boto3
-import botocore
-from flask import Flask, render_template,request
-from flask import url_for
-from flask_bootstrap import Bootstrap
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
-from aws_xray_sdk.core import xray_recorder, patch_all
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
-# from flask_mysqldb import MySQL
-# import MySQLdb.cursors
-import re
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python38_render_template]
+# [START gae_python3_render_template]
+import datetime
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-xray_recorder.configure(service='My Flask application')
-XRayMiddleware(app, xray_recorder)
-
-patch_all()
-bootstrap = Bootstrap(app)
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'root'
-# app.config['MYSQL_DB'] = 'bitcoin'
-  
-# mysql = MySQL(app)
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-@app.route('/faq')
-def faq():
-    return render_template('faq.html')
-@app.route('/blog')    
-def blog():
-    return render_template('blog.html')
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-@app.route('/pricing')
-def pricing():
-    return render_template('pricing.html')
-    
-@app.route('/login',methods=['GET', 'POST'])
-def login():
-    # return render_template('login.html')
-    msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # cursor.execute('SELECT * FROM user WHERE username = % s AND password = % s', (username, password, ))
-        # account = cursor.fetchone()
-        if True:
-            # session['loggedin'] = True
-            # session['id'] = account['id']
-            # session['username'] = account['username']
-            msg = 'Logged in successfully !'
-            return render_template('index.html', msg = msg)
-        else:
-            msg = 'Incorrect username / password !'
-    return render_template('login.html', msg = msg)
-@app.route('/signup',methods=['GET', 'POST'])
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
 
-def signup():
-    msg = ''
-   
-    if request.method == 'POST' and 'firstname' in request.form and 'lastname' in request.form and 'username' in request.form and 'password' in request.form and 'email' in request.form :        
-        username = request.form['username']
-        password = request.form['password']
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email']
-        # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # cursor.execute('SELECT * FROM user WHERE username = % s', (username, ))
-        # account = cursor.fetchone()
-        # if account:
-            # msg = 'Account already exists !'
-        # elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            # msg = 'Invalid email address !'
-        # elif not re.match(r'[A-Za-z0-9]+', username):
-            # msg = 'Username must contain only characters and numbers !'
-        # elif not username or not password or not email:
-            # msg = 'Please fill out the form !'
-        # else:
-            # cursor.execute('INSERT INTO user VALUES (NULL,%s,%s, % s, % s, % s)', (firstname,lastname,username, password, email, ))
-            # mysql.connection.commit()
-        msg = 'You have successfully registered !'
-    # elif request.method == 'POST':
-        # msg = 'Please fill out the form !'
-    return render_template('signup.html', msg = msg) 
+    return render_template('index.html', times=dummy_times)
 
- if __name__ == '__main__':
+
+if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
@@ -100,4 +43,4 @@ def signup():
     # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python3_render_template]
-# [END gae_python38_render_template] 
+# [END gae_python38_render_template]
